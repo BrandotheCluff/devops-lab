93dfff085aa485e0cdda09fd2f15867c83849423
BrandotheCluff <brandon.cluff2@gmail.com>
2022-04-28 16:13:40 -0600

    roll out dev

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
 const app = express()
 app.use(express.json())
 
-// app.get('/', (req,res) => {
-//     res.sendFile(path.join(__dirname, '../index.html'))
-//     rollbar.info('html file served successfully.')
-// })
+rollbar = new Rollbar({
+    accessToken: 'd4f7e3bb10e543f0ad89e4ca293c905a',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  })
+  rollbar.log('Hello world!')
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('html file served successfully.')
+})
 
 app.get('/style', (req, res) => {
     res.sendFile(path.join(__dirname, '../styles.css'))
