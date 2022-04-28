e4daf997f73155004b36e2f97b96878677cf65de
BrandotheCluff <brandon.cluff2@gmail.com>
2022-04-28 16:03:48 -0600

    setup part 2

--- server/index.js
+++ server/index.js
@@ -3,6 +3,20 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('html file served successfully.')
+})
+
+app.get('/style', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+    rollbar.info('css file served')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
 
 
 
