5da17f37855976fc0464e0d6e10008b55d55fe8f
BrandotheCluff <brandon.cluff2@gmail.com>
2022-04-28 15:53:48 -0600

    dev to tyrants

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+app.use(express.json())
+
+
+
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () =>  console.log(`Operating on humility ${port}`))
